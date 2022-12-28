14ea4dbbd014ab5473fe0db86071eee8d434997a
Mansoor Khan <thisismansoorkhan@gmail.com>
2022-12-28 18:17:27 +0530

    Initial Commit

---
+++ Music Wusic/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Music Wusic
+//
+//  Created by Mansoor Khan on 28/12/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
